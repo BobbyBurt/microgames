263419be3c9d368678ce9c9e2254063e1cb81a82
BobbyBurt <aarontburt@gmail.com>
2022-03-28 18:59:01 -0400

    microgame scene setup

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // import Phaser from './lib/phaser.js'
 
-import game from './scenes/game.js'
+import microgame from './scenes/microgame.js'
 import boot from './scenes/boot.js'
 import menu from './scenes/menu.js'
 import timer from './scenes/timer.js'
@@ -24,5 +24,5 @@ window.game = new Phaser.Game({
         height: window.innerHeight * window.devicePixelRatio,
         zoom: 1 / window.devicePixelRatio
     },
-    scene: [boot, game, menu, timer]
+    scene: [boot, microgame, menu, timer]
     });
---
+++ src/scenes/microgame.js
@@ -0,0 +1,60 @@
+import eventsCenter from "../eventsCenter.js";
+
+/**
+ * this was the first microgame I tried my hand at making. An icebreaker, if you will
+ */
+
+export default class game extends Phaser.Scene
+{
+    constructor()
+    {
+        super('microgame'); // defining unique key
+    }
+
+    // configure scene
+    init()
+    {
+        
+    }
+
+    // queue assets to load
+    preload()
+    {
+
+    }
+
+    create()
+    {
+        // HINT
+
+        this.hint = this.add.text(0, 0, HINT, {fontSize: 48});
+        this.hint.setStroke('#262626', 20);
+        this.hint.setOrigin(0.5, 0.5);
+    
+        this.hintTimer = this.time.delayedCall(1000, () => {
+
+            this.hint.visible = false;
+        });
+
+        // TIMER END
+
+        eventsCenter.on('timer-end', () =>
+        {
+            this.scene.stop(this.scene.key);
+        });
+
+        // RESIZE
+
+        this.resizeField(this.scale.width, this.scale.height);
+    }
+
+    update()
+    {
+
+    }
+
+    resizeField(w, h)
+    {
+        
+    }
+}
